eae5c4665404151645c86e3af64640e942725655
ElMrabetHossam <hossam.elmrabet@uit.ac.ma>
2025-05-01 20:26:38 +0100

    ajout de la navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/navbar";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    
-    <div className="text-9xl font-bold underline flex justify-center text-center bg-cyan-600">
-      Hello!  
-    </div>
-  )
+    <>
+      <Navbar />
+    </>
+  );
 }
 
-export default App
+export default App;
